0316d47da7dd1f3445ae257b3dac7272b4b8df81
Igor <igor.parfianets@gmail.com>
2023-07-26 22:44:33 +0300

    Add TinyUrlContext and Entities to Database layer

---
+++ WebApi/TinyUrl.Database/TinyUrlContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TinyUrl.Database
+{
+    public class TinyUrlContext : DbContext
+    {
+        public TinyUrlContext(DbContextOptions<TinyUrlContext> options) : base(options)
+        {
+        }
+    }
+}
