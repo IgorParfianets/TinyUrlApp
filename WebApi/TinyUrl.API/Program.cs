99f91f577cec62f936a24f06230016673fd6b12a
Igor <igor.parfianets@gmail.com>
2023-07-26 23:09:13 +0300

    Set configurations for run app and set nuget packages

--- WebApi/TinyUrl.API/Program.cs
+++ WebApi/TinyUrl.API/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
+using System.Reflection;
+using System.Text;
+using TinyUrl.Database;
+
 namespace TinyUrl.API
 {
     public class Program
@@ -6,6 +13,26 @@ namespace TinyUrl.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Host.UseSerilog((ctx, lc) => lc
+                .WriteTo.Console()
+                .WriteTo.File(GetPathToLogFile(),
+                    LogEventLevel.Information));
+
+            string myCorsPolicyName = "ReactApp";
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(myCorsPolicyName, policyBuilder =>
+                {
+                    policyBuilder
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowAnyOrigin();
+                });
+            });
+
+            var connectionString = builder.Configuration.GetConnectionString("Default");
+            builder.Services.AddDbContext<TinyUrlContext>(
+                optionBuilder => optionBuilder.UseSqlServer(connectionString));
             // Add services to the container.
 
             builder.Services.AddControllers();
@@ -30,6 +57,20 @@ namespace TinyUrl.API
             app.MapControllers();
 
             app.Run();
+        }
+
+        /// <summary>
+        ///     Returns the path for log file recording.
+        /// </summary>
+        /// <returns>A string whose value contains a path to the log file</returns>
+        private static string GetPathToLogFile()
+        {
+            var sb = new StringBuilder();
+            sb.Append(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            sb.Append(@"\logs\");
+            sb.Append($"{DateTime.Now:yyyyMMddhhmmss}");
+            sb.Append("data.log");
+            return sb.ToString();
         }
     }
 }
